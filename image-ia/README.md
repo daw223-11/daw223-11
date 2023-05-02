274c34174d7eb2564d3be3c1109bfab9f983739a
daw211 <alfonso.ravelo.gil@iesjulianmarias.es>
2023-05-02 22:55:05 +0200

    Cambios en el header. Intranet añadida no funcional

---
+++ image-ia/README.md
@@ -0,0 +1,2 @@
+# Imágenes generadas por DALL-E
+## Los promp usados para generar las imágenes son el título de las que tienen las mismas
---
+++ wordpress/intranet/resources/js/App.jsx
@@ -0,0 +1,11 @@
+import "./bootstrap";
+import "./components/HolaMundo";
+import React from "react";
+
+export default function App() {
+    return (
+        <div>
+            <HolaMundo />
+        </div>
+    );
+}
