ae5b1a0b6092de8c4df523979ae5d028e7fd13e7
daw223-11 <alfonso.ravelo.gil@iesjulianmarias.es>
2023-05-04 21:01:55 +0000

    Proyecto-Repositorio de react subido. Front desacoplado y listo para ser usado. Eliminado /api como prefix.

--- web/intranet/intranet-api/app/Providers/RouteServiceProvider.php
+++ web/intranet/intranet-api/app/Providers/RouteServiceProvider.php
@@ -30,11 +30,12 @@ class RouteServiceProvider extends ServiceProvider
 
         $this->routes(function () {
             Route::middleware('api')
-                ->prefix('api')
                 ->group(base_path('routes/api.php'));
 
-            Route::middleware('web')
-                ->group(base_path('routes/web.php'));
+
+            // Si es necesario manejar las rutas de las vistas descomentar código
+            /* Route::middleware('web')
+                ->group(base_path('routes/web.php')); */
         });
     }
 }
